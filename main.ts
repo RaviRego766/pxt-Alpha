cf7147bc3a05614741953ed5323a7ae06f8b6703
RaviRego766 <jfrancarego@gmail.com>
2024-08-10 15:57:38 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,7 @@ enum operator{
     //% block="F"
     zeroFillRightShift
 }
-//% weight=20 color=#ff0890 icon="\uf101" block="Alpha"
+//% weight=20 color=#ff0890 icon="\u16800" block="Alpha"
 namespace ModeAlpha {
 
     
